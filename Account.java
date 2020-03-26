2f946522dd8e110bbdd4bcbef96d2cf2ac6c16b2
milz L <13milesj@gmail.com>
2020-03-25 21:19:56 -0400

    fixing the account class

--- Account.java
+++ Account.java
@@ -1,23 +1,5 @@
 package TheaterSystem;
-<<<<<<< HEAD
 
-/*
- * The account class
- * Miles Littlejohn
- */
-public class Account {
-	public String firstName;
-	private String lastName;
-	private int ticketsPurchased;
-
-	/*
-	 * Creates an account with inputed first and last names
-	 */
-	public Account(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-=======
 /**
  * 
  * @author Miles
@@ -38,5 +20,5 @@ public class Account {
     this.firstName = firstName;
     this.lastName = lastName;
   }
->>>>>>> 82f9e899b74d7c04cc08233383a144de126e3b88
+
 }
