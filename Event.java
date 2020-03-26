a512cb2b020ffa07f26081a583d95fe87fb07932
ianm24 <ianmcdowell24@gmail.com>
2020-03-25 20:38:00 -0400

    Filled in Event class

--- Event.java
+++ Event.java
@@ -6,5 +6,32 @@ package TheaterSystem;
  *
  */
 public class Event {
-
+	public Venue venue;
+	public Theater theater;
+	public String startTime;
+	public String endTime;
+	
+	/**
+	 * Used to create a new Event
+	 * 
+	 * @param venue The venue the event takes place
+	 * @param theater The theater venue the event takes place
+	 * @param startTime The start time of the event
+	 * @param endTime The end time of the event
+	 */
+	public Event(Venue venue, Theater theater, String startTime, String endTime) {
+		this.venue = venue;
+		this.theater = theater;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	/**
+	 * Returns all the information about the Event
+	 */
+	public String toString() {
+		return "Venue: " + this.venue + "\nTheater: " +
+	this.theater + "\nStart Time: " + this.startTime +
+	"\nEnd Time: " + this.endTime;
+	}
 }
