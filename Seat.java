f0c26f2244870e1b8822500802ab8f04f89e50e8
Tyron Schultz <tyronsonfire@gmail.com>
2020-03-25 20:37:27 -0400

    finished venue and seat

--- Seat.java
+++ Seat.java
@@ -1,5 +1,18 @@
 package TheaterSystem;
 
 public class Seat {
-
+	public boolean isHandicapable;
+	public boolean isReserved;
+	public char row;
+	public int col;
+	
+	public Seat(boolean isHandicapable, char row, int col) {
+		this.isHandicapable = isHandicapable;
+		this.row = row;
+		this.col = col;
+	}
+	
+	public void setReserved(boolean isReserved) {
+		this.isReserved = isReserved;
+	}
 }
--- Venue.java
+++ Venue.java
@@ -1,5 +1,17 @@
 package TheaterSystem;
 
+import java.util.ArrayList;
 public class Venue {
-
+	private ArrayList<Theater> theaters;
+	private String name;
+	
+	public Venue(String name) {
+		this.name = name;
+		this.theaters = new ArrayList<Theater>();
+	}
+	
+	public void addTheater(ArrayList<Seat> seats) {
+		Theater theater = new Theater(seats, this);
+		this.theaters.add(theater);
+	}
 }
