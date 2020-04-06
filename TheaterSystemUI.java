a8e5ff0ad238507a0b2def63c51dc7441d678cfd
milz L <13milesj@gmail.com>
2020-04-05 21:43:57 -0400

    fixing systemUI

--- TheaterSystemUI.java
+++ TheaterSystemUI.java
@@ -203,7 +203,7 @@ public class TheaterSystemUI {
 	}
 
 	private void displayEmployee() {
-<<<<<<< HEAD
+
 	  while(quit == false) {
         System.out.println("Welcome, "+ts.currentAccount.firstName+", please select from the following "
             + "options:\n1. Display the list of events\n2. Search for events by name\n3. Search for events"
@@ -215,30 +215,41 @@ public class TheaterSystemUI {
           case 1:
             tsUI.displayEventList();
             break;
+            
           case 2:
             tsUI.searchByName();
             break;
+            
           case 3:
             tsUI.searchByGenre();
             break;
+            
           case 4:
             tsUI.searchByRating();
             break;
+            
           case 5:
             tsUI.displayEventInfo();
             break;
+            
           case 6:
             tsUI.purchaseTicket();
             break;
-          case 7:
             
+          case 7:
+            tsUI.getRefund();
             break;
+            
           case 8:
+           tsUI.leaveReview();
             break;
+            
           case 9:
             break;
+            
           case 10:
             break;
+            
           case 11:
             quit = true;
             break;
@@ -247,12 +258,11 @@ public class TheaterSystemUI {
             break;
         }
       }
-=======
 	  
->>>>>>> 2cc4787bd2fe56990848fb63c42f57cbe43fd273
 	}
 
 	private void displayAdmin() {
+	  
 	  while(quit == false) {
         System.out.println("Welcome, Admin: " + ts.currentAccount.firstName + "!\nPlease select from the following options:\n1. Display the list of events"
            + "\n2. Search for events by name\n3. Search for events by genre\n4. Search for events by rating\n5. Display an event's information"
@@ -312,7 +322,8 @@ public class TheaterSystemUI {
             System.out.println("Invalid Selection");
             break;
         }
-      } 
+      }
+	  
 	}
 
 	private void searchByName() {
